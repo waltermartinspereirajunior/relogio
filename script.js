33ece27ba873491c4ff2e865acfaeba80fa38245
walter <walter@walter>
2023-06-21 21:42:49 -0300

    relogio e cronometro

---
+++ script.js
@@ -0,0 +1,80 @@
+const getHours = () => {
+    const clock = document.getElementsByClassName('clock')[0]
+    const date = new Date()
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+    const hour = hours < 10 ? `0${hours}` : hours
+    const minute = minutes < 10 ? `0${minutes}` : minutes
+    const second = seconds < 10 ? `0${seconds}` : seconds
+    clock.innerHTML = `${hour}:${minute}:${second}`
+  }
+  
+  setInterval(() => {
+    getHours()
+  }, 1000)
+
+  "use strict";
+
+  var centesimas = 0;
+  var segundos = 0;
+  var minutos = 0;
+  var horas = 0;
+  function inicio () {
+      control = setInterval(cronometro,10);
+      document.getElementById("inicio").disabled = true;
+      document.getElementById("parar").disabled = false;
+      document.getElementById("continuar").disabled = true;
+      document.getElementById("reinicio").disabled = false;
+  }
+  function parar () {
+      clearInterval(control);
+      document.getElementById("parar").disabled = true;
+      document.getElementById("continuar").disabled = false;
+  }
+  function reinicio () {
+      clearInterval(control);
+      centesimas = 0;
+      segundos = 0;
+      minutos = 0;
+      horas = 0;
+      Centesimas.innerHTML = ":00";
+      Segundos.innerHTML = ":00";
+      Minutos.innerHTML = ":00";
+      Horas.innerHTML = "00";
+      document.getElementById("inicio").disabled = false;
+      document.getElementById("parar").disabled = true;
+      document.getElementById("continuar").disabled = true;
+      document.getElementById("reinicio").disabled = true;
+  }
+  function cronometro () {
+      if (centesimas < 99) {
+          centesimas++;
+          if (centesimas < 10) { centesimas = "0"+centesimas }
+          Centesimas.innerHTML = ":"+centesimas;
+      }
+      if (centesimas == 99) {
+          centesimas = -1;
+      }
+      if (centesimas == 0) {
+          segundos ++;
+          if (segundos < 10) { segundos = "0"+segundos }
+          Segundos.innerHTML = ":"+segundos;
+      }
+      if (segundos == 59) {
+          segundos = -1;
+      }
+      if ( (centesimas == 0)&&(segundos == 0) ) {
+          minutos++;
+          if (minutos < 10) { minutos = "0"+minutos }
+          Minutos.innerHTML = ":"+minutos;
+      }
+      if (minutos == 59) {
+          minutos = -1;
+      }
+      if ( (centesimas == 0)&&(segundos == 0)&&(minutos == 0) ) {
+          horas ++;
+          if (horas < 10) { horas = "0"+horas }
+          Horas.innerHTML = horas;
+      }
+  }
